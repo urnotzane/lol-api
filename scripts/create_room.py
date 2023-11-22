5200b350ffcf45e347a9627477c1a5e984bf26f0
urnotzane <kaylezang@tencent.com>
2023-11-22 10:28:57 +0800

    feat: 移动文件

--- scripts/create_room.py
+++
@@ -1,5 +0,0 @@
-# 根据 queueId 创建常规房间
-# 参数和房间数据：https://github.com/XHXIAIEIN/LeagueCustomLobby#queue-maps-game-modes-game-types
-async def createLobby(connection):
-  queue = {'queueId': 420}
-  await connection.request('POST', '/lol-lobby/v2/lobby', data=queue)
